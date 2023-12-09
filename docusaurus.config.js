6bcd68a02f1cbd04b63accf9b5013571d8ae10c6
aireugene <87520230+aireugene@users.noreply.github.com>
2023-12-10 00:46:36 +0900

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // ...
+  url: 'https://aireugene.github.io', // GitHub organization 또는 username + github.io
+  baseUrl: '/Tatum_KB/', // '/projectName/' 포맷으로 깃헙 레포지토리 이름을 입력한다.
+  projectName: 'Tatum_KB',
+  organizationName: 'aireugene', // GitHub organization 또는 username
+  // ...
+};
